2186f358f42904b685512966af1d26b09f608a30
shvms <dev.shvms@gmail.com>
2024-03-16 14:48:32 +0530

    init

---
+++ src/main/java/in/dev/shvms/multithreading/MultithreadingApplication.java
@@ -0,0 +1,13 @@
+package in.dev.shvms.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingApplication.class, args);
+	}
+
+}
